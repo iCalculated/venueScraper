2ddab15fa5c11d9a7de8202c9cfb36cb87dffaa4
iCalculated <fufa0001@gmail.com>
2021-08-10 12:55:53 -0500

    more functions!

--- scraper.js
+++ scraper.js
@@ -30,22 +30,24 @@ const search_list = async (places, city) => {
     return results;
 }
 
-const format = ({name, location, categories, hours, image_url, coordinates, price, phone, photos})=> {
+const csvFormat = ({name, location, categories, hours, image_url, coordinates, price, phone, photos})=> {
     const formatted_hours = Array(7).fill("null");
 
-    hours
-        .filter(hours => hours.hours_type == 'REGULAR')[0]
-        .open
-        .forEach(day_schedule => {
-            formatted_hours[day_schedule.day] = day_schedule.start
-                ? `{open: "` + day_schedule.start + `", close: "` + day_schedule.end +`"}` 
-                : `null`
-        });
+    if(hours) {
+        hours
+            .filter(hours => hours.hours_type == 'REGULAR')[0]
+            .open
+            .forEach(day_schedule => {
+                formatted_hours[day_schedule.day] = day_schedule.start
+                    ? `{open: "` + day_schedule.start + `", close: "` + day_schedule.end +`"}` 
+                    : `null`
+            });
+    }
 
     return {
         name,
         address: location.address1 + ",\n" + location.city + ", " + location.state + " " + location.zip_code,
-        descrption: null,
+        description: null,
         types: null,
         tags: categories.map(tag => tag.alias).join(","),
         sunday: formatted_hours[6],
@@ -60,8 +62,43 @@ const format = ({name, location, categories, hours, image_url, coordinates, pric
         longitude: coordinates.longitude,
         price,
         phone,
-        photots: photos.join(","),
+        photos: photos.join(","),
         //extendedProps
+    };
+};
+
+const format = ({name, location, categories, hours, image_url, coordinates, price, phone, photos, ...extendedProps})=> {
+    const formatted_hours = Array(7).fill("null");
+
+    if (hours) {
+        hours
+            .filter(hours => hours.hours_type == 'REGULAR')[0]
+            .open
+            .forEach(day_schedule => {
+                formatted_hours[day_schedule.day] = { 
+                    open: day_schedule.start, 
+                    close: day_schedule.end
+                };
+            });
+
+        // match day indexing
+        formatted_hours.unshift(
+            formatted_hours.pop()
+        );
+    }
+
+    return {
+        name,
+        address: location.address1 + ",\n" + location.city + ", " + location.state + " " + location.zip_code,
+        tags: categories.map(tag => tag.alias).join(","),
+        hours: formatted_hours,
+        image_link: image_url,
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude,
+        price,
+        phone,
+        photos: photos.join(","),
+        extendedProps
     };
 };
 
@@ -72,6 +109,20 @@ const convertToCSV = objectArray => {
             return typeof value === 'string' ? JSON.stringify(value) : value
         }).toString()
     }).join('\n')
+}
+
+const scrape_to_CSV = (places, city, file) => {
+    search_list(places, city)
+        .then(results => results.map(csvFormat))
+        .then(convertToCSV)
+        .then(csv => {
+            fs.writeFile(file, csv, err => {
+                if (err) {
+                console.error(err)
+                return
+                }
+            })
+        });
 }
 
 const client = yelp.client(process.env.YELP_API_KEY)
@@ -91,15 +142,5 @@ const places = [
 ];
 const city = "Austin, TX";
 
-search_list([places[1]], city)
-// don't fill days from 0, use day field
-    .then(results => results.map(format))
-    .then(convertToCSV)
-    .then(csv => {
-        fs.writeFile('./export.csv', csv, err => {
-            if (err) {
-            console.error(err)
-            return
-            }
-        })
-    });
+
+scrape_to_CSV(places, city, './export.csv')
